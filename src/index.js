891383f09dd8520590015c1304f4d94d5161663c
Your Name <you@example.com>
2018-02-22 22:08:18 +0300

    money-exchange decided

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if(currency >= 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+     
+    let k = 0, 
+    		obj = {"H" : 50, "Q" : 25, "D" : 10, "N" : 5, "P" : 1};
+    
+    for(let key in obj){
+
+    	k = obj[key];
+    	obj[key] = Math.floor(currency / obj[key]);
+      
+        if(obj[key] <= 0) delete obj[key];
+        else currency = currency - obj[key]*k;
+    }
+    
+    return obj;
 }
