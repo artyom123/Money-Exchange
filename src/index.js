aee4f24d9a0964322dd3f07d7b614a601b8953fe
artyom123 <artvirsh@mail.ru>
2018-09-11 21:23:21 +0300

    Edit decision

--- src/index.js
+++ src/index.js
@@ -2,17 +2,16 @@
 module.exports = function makeExchange(currency) {
     if(currency >= 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
      
-    let k = 0, 
-    		obj = {"H" : 50, "Q" : 25, "D" : 10, "N" : 5, "P" : 1};
+    const obj = {"H" : 50, "Q" : 25, "D" : 10, "N" : 5, "P" : 1};
+    let objFinal = {};
     
-    for(let key in obj){
+    for(let key in obj) {
 
-    	k = obj[key];
-    	obj[key] = Math.floor(currency / obj[key]);
+    	objFinal[key] = Math.floor(currency / obj[key]);
       
-        if(obj[key] <= 0) delete obj[key];
-        else currency = currency - obj[key]*k;
+        if(objFinal[key] <= 0) delete objFinal[key];
+        else currency -= obj[key] * objFinal[key];
     }
     
-    return obj;
+    return objFinal;
 }
